eddd9730d04866bd932416d56cfc55fc75ecacfd
dhruvmakwana <dmakwana503@gmail.com>
2021-08-13 18:45:22 +0530

    added all files

---
+++ app.py
@@ -0,0 +1,17 @@
+# importing libraries
+from flask import Flask, request
+from run import main
+
+app = Flask(__name__)
+
+@app.route("/upload/facemesh")
+def facemesh():
+    image = request.args.get('image')
+    distance_dict, angle_dict = main(image)
+    output = {}
+    output["distance_dict"] = distance_dict
+    output["angle_dict"] = angle_dict
+    return {"data": output}
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 80, debug = True)
